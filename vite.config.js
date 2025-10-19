626ddbea99a42d06fec43bc0b4c38660e89212b1
Darshangohel2610 <darshangohel32@gmail.com>
2025-10-19 12:29:50 +0530

    removed base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'dass-frontend-react',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
